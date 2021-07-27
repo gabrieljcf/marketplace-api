f886a511570a915f5d7b54984c8093be27c32adb
Gabriel Nogueira <gabrieljcf@hotmail.com>
2021-07-27 00:30:50 -0300

    feature: add container for dependenci inversion

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,7 @@
 import { container } from "tsyringe";
 
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
 import { ICategoriesRepository } from "../../modules/category/repositories/ICategoriesRepository";
 import { CategoriesRepository } from "../../modules/category/repositories/implementations/CategoriesRepository";
 import { ProductsRepository } from "../../modules/products/repositories/implementations/ProductsRepository";
@@ -13,4 +15,9 @@ container.registerSingleton<IProductsRepository>(
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
   CategoriesRepository
+);
+
+container.registerSingleton<IUsersRepository>(
+  "UsersRepository",
+  UsersRepository
 );
