23f6fc311fa11abdeca0745d7f41b51752d3986b
Gabriel Nogueira <gabrieljcf@hotmail.com>
2022-01-28 12:39:09 -0300

    chore: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express from "express";
 import "express-async-errors";
 import "reflect-metadata";
@@ -8,6 +9,7 @@ import { routes } from "./routes";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 app.use(exceptionErrorHandling);
 
