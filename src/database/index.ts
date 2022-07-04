940dabb4bb3bb500efa76e52e04d46c86c7ff348
Gabriel Nogueira <gabrieljcf@hotmail.com>
2022-07-04 00:50:55 -0300

    feat: adjust mongoose connection

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import "dotenv/config";
 
+mongoose.set("useCreateIndex", true);
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
