80fc5a3558c25ad6a6dbc0456e88e3410849a095
Gabriel Nogueira <gabrieljcf@hotmail.com>
2023-04-03 15:16:42 -0300

    fix: adjust types

--- src/modules/category/usecases/listCategories/ListCategoriesController.ts
+++ src/modules/category/usecases/listCategories/ListCategoriesController.ts
@@ -1,8 +1,16 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 import { container } from "tsyringe";
 
 import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
+type Request = {
+  query: {
+    isActive?: boolean;
+    name?: string;
+    page?: number;
+    limit?: number;
+  };
+};
 class ListCategoriesController {
   public async handle(request: Request, response: Response): Promise<Response> {
     const { isActive, name, page, limit } = request.query;
